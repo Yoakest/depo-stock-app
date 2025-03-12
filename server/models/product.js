03828d55be0fd6e2a3e5954a8c0117a1399694e7
Yoake <shafaktm@gmail.com>
2025-03-12 12:35:10 +0300

    database düzenlemeleri

--- server/models/product.js
+++ server/models/product.js
@@ -1,50 +1,48 @@
 module.exports = (sequelize, DataTypes) => {
-    const Product = sequelize.define('Product', {
-      product_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      product_code: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      brand: {
-        type: DataTypes.STRING,
-      },
-      pallet_quantity: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0,
-      },
-      package_quantity: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0,
-      },
-      weight: {
-        type: DataTypes.DECIMAL,
-      },
-      total_stock: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0,
-      },
-      total_pallets: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0,
-      },
-      category_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'Categories',
-          key: 'id',
-        },
-      },
+  const Product = sequelize.define('Product', {
+    product_name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    product_code: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+      unique: true,
+    },
+    brand: {
+      type: DataTypes.STRING(25),
+    },
+    pallet_quantity: {
+      type: DataTypes.STRING(20),
+      defaultValue: 0,
+    },
+    package_quantity: {
+      type: DataTypes.STRING(20),
+      defaultValue: 0,
+    },
+    weight: {
+      type: DataTypes.STRING(20),
+    },
+    total_stock: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    total_pallets: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    hide: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: 0
+    }
+  });
+
+  Product.associate = (models) => {
+    Product.belongsToMany(models.Category, {
+      through: models.ProductCategory,
+      foreignKey: "product_id",
     });
-  
-    Product.associate = (models) => {
-      // Ürünün bir kategorisi olabilir
-      Product.belongsTo(models.Category, { foreignKey: 'category_id' });
-    };
-  
-    return Product;
   };
-  
+
+  return Product;
+};

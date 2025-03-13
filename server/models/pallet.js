77fddcce0ae7813249f00346a89e75e63fb4bf54
Yoake <shafaktm@gmail.com>
2025-03-13 17:11:29 +0300

    Veritabanı düzenlemeleri

--- server/models/pallet.js
+++ server/models/pallet.js
@@ -1,32 +1,37 @@
 module.exports = (sequelize, DataTypes) => {
-    const Pallet = sequelize.define('Pallet', {
-      pallet_no: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      product_code: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      product_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      quantity: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      location: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    });
-  
-    Pallet.associate = (models) => {
-      // Her paletin bir sevkiyat bilgisi olabilir
-      Pallet.belongsTo(models.Shipment, { foreignKey: 'shipment_id' });
-    };
-  
-    return Pallet;
+  const Pallet = sequelize.define('Pallet', {
+    pallet_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    pallet_no: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    product_code: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+    },
+    product_name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.ENUM('Pharmastar', 'Logismart'),
+      allowNull: false,
+      defaultValue: 'Pharmastar'
+    }
+  });
+
+  Pallet.associate = (models) => {
+    // Her paletin bir sevkiyat bilgisi olabilir
+    Pallet.belongsTo(models.Shipment, { foreignKey: 'shipment_id' });
   };
-  
+
+  return Pallet;
+};
